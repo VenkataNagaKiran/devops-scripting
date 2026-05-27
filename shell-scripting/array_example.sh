579d23e1c1631fa581d39bf90d4cf90e5a1274bb
Mavuri <venkata.naga.kiran.mavuri@sap.com>
2026-05-27 19:15:16 +0530

    Added array_example.sh script

---
+++ shell-scripting/array_example.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Name[0]="HITMAN ROHIT"
+Name[1]="KING KOHLI"
+Name[2]="ABHISHEK SHARMA"
+Name[3]="ISHAN KISHAN"
+Name[4]="JASSI BHAI"
+Name[5]="KUNG FU PANDYA"
+NAME[6]="CAPTAIN COOL"
+NAME[7]="RISHAB PANT"
+Name[8]="SHREYAS IYER"
+Name[9]="RAVINDRA JADEJA"
+Name[10]="BHUVI"
+Name[11]="SHIVAM DUBE"
+
+echo "The opening batsmen are , ${Name[0]} and ${Name[2]}"
+echo "The squad is ${Name[*]}"
+echo "The team is ${Name[@]}"
--- shell-scripting/sp_variable.sh
+++ shell-scripting/sp_variable.sh
@@ -3,6 +3,9 @@
 # For loop works with the combination of do and done in shell scripting
 # $* refers to our scripts own arguments
 
+# set -x runs the script like debug mode
+set -x
+
 for TOKEN in $*
 do
 	echo "$TOKEN"
